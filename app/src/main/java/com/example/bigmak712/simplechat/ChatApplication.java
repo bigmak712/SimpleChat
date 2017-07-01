26e60a61e9bcea6747f80ecaadfba2d1d85ff311
bigmak712 <bigmak712@gmail.com>
2017-07-01 16:00:41 -0700

    finished bonus features

--- app/src/main/java/com/example/bigmak712/simplechat/ChatApplication.java
+++ app/src/main/java/com/example/bigmak712/simplechat/ChatApplication.java
@@ -4,7 +4,10 @@ import android.app.Application;
 import android.content.res.Configuration;
 
 import com.parse.Parse;
-import com.parse.interceptors.ParseLogInterceptor;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
  * Created by bigmak712 on 6/1/17.
@@ -15,13 +18,27 @@ public class ChatApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Message.class);
+
         // Required initialization logic here!
         // set applicationId and server based on the values in the Heroku settings.
         // any network interceptors must be added with the Configuration Builder given this syntax
+
+        // Use for monitoring Parse network traffic
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId and server based on the values in the Heroku settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("93bd0b59a90d46b1999e484431b83f41") // should correspond to APP_ID env variable
-                .addNetworkInterceptor(new ParseLogInterceptor())
-                .server("https://simplechatclient.herokuapp.com/parse/").build());
+                .applicationId("simple_chat_lab") // should correspond to APP_ID env variable
+                .clientBuilder(builder)
+                .server("https://simple-chat-lab.herokuapp.com/parse/").build());
+
     }
 
     // Called by the system when the device configuration changes while your component is running.
